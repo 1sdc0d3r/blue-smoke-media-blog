0c2bae736259c8180ffe85b7d276d7a020f27137
Braden Bell <fish0859@gmail.com>
2020-10-06 10:33:54 -0600

    fixed spacing on all articles

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,1 +1,1 @@
-export default ["https://blue-smoke-blog.herokuapp.com/api/blog", "http://localhost:5000"]
+export default ["http://localhost:5000/api/blog/", "https://blue-smoke-blog.herokuapp.com/api/blog"]
