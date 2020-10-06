a85faba839cb6c3f6b85363f75f2d997d085591d
Braden Bell <fish0859@gmail.com>
2020-10-06 16:33:15 -0600

    moved helmet to proper place

--- client/src/components/ArticleView.jsx
+++ client/src/components/ArticleView.jsx
@@ -33,82 +33,97 @@ export default function Article({ history }) {
   return (
     <>
       {post ? (
-        <article>
-          <img
-            src={`https://blue-smoke-media.s3-us-west-1.amazonaws.com/Blog/primary/${post.imageUrl}`}
-            alt={post.imageAlt}
-            className="primary-img"
-          />
-          <h1>{post.title}</h1>
-          {/* todo check with CoBe */}
-          <pre className="content">{ReactHtmlParser(post.content)}</pre>
-          <div className="share-info">
-            <div className="share">
-              <ul>
-                Share:
-                <li>
-                  <a
-                    href={`https://facebook.com/sharer/sharer.php?u=${encodedURL}`}
-                  >
-                    <img src={facebook} alt="facebook icon" className="icon" />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href={`https://twitter.com/intent/tweet?text=${encodedURL}`}
-                    data-size="large"
-                  >
-                    <img src={twitter} alt="twitter icon" className="icon" />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodedURL}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src={linkedIn} alt="LinkedIn icon" className="icon" />
-                  </a>
-                </li>
-                <li>
-                  <a href={`mailto:?subject=${post.title}&body=${encodedURL}`}>
-                    <FiMail color="#1e92f6" />
-                  </a>
-                </li>
-              </ul>
+        <>
+          <article>
+            <img
+              src={`https://blue-smoke-media.s3-us-west-1.amazonaws.com/Blog/primary/${post.imageUrl}`}
+              alt={post.imageAlt}
+              className="primary-img"
+            />
+            <h1>{post.title}</h1>
+            {/* todo check with CoBe */}
+            <pre className="content">{ReactHtmlParser(post.content)}</pre>
+            <div className="share-info">
+              <div className="share">
+                <ul>
+                  Share:
+                  <li>
+                    <a
+                      href={`https://facebook.com/sharer/sharer.php?u=${encodedURL}`}
+                    >
+                      <img
+                        src={facebook}
+                        alt="facebook icon"
+                        className="icon"
+                      />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={`https://twitter.com/intent/tweet?text=${encodedURL}`}
+                      data-size="large"
+                    >
+                      <img src={twitter} alt="twitter icon" className="icon" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodedURL}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        src={linkedIn}
+                        alt="LinkedIn icon"
+                        className="icon"
+                      />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={`mailto:?subject=${post.title}&body=${encodedURL}`}
+                    >
+                      <FiMail color="#1e92f6" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="info">
+                <h4>Author {post.date ? "• Date" : ""}</h4>
+                <p className="author">
+                  <Link to={`/author/${post.author}`}>{post.author}</Link>
+                  {post.date ? ` • ${post.date}` : ""}
+                </p>
+                <h4>Category</h4>
+                <p>
+                  <Link to={`/category/${post.category}`}>
+                    {" "}
+                    {post.category}
+                  </Link>
+                </p>
+              </div>
             </div>
-            <div className="info">
-              <h4>Author {post.date ? "• Date" : ""}</h4>
-              <p className="author">
-                <Link to={`/author/${post.author}`}>{post.author}</Link>
-                {post.date ? ` • ${post.date}` : ""}
-              </p>
-              <h4>Category</h4>
+            <div className="tags">
+              <h4>Tags</h4>
               <p>
-                <Link to={`/category/${post.category}`}> {post.category}</Link>
+                {post.tags.split(",").map((e, i) => (
+                  <Link to={`/tags/${e}`}>{i !== 0 ? ` • ${e}` : e}</Link>
+                ))}
               </p>
             </div>
-          </div>
-          <div className="tags">
-            <h4>Tags</h4>
-            <p>
-              {post.tags.split(",").map((e, i) => (
-                <Link to={`/tags/${e}`}>{i !== 0 ? ` • ${e}` : e}</Link>
-              ))}
-            </p>
-          </div>
 
-          {/* //todo category tags here */}
-        </article>
+            {/* //todo category tags here */}
+          </article>
+          <Helmet>
+            <title>{post.title}</title>
+            <meta name="keywords" content={post.tags} />
+            <meta name="description" content={post.snippet} />
+            <meta name="author" content={post.author} />
+          </Helmet>
+        </>
       ) : (
         <p id="loading">{loadingMessage}</p>
       )}
-      <Helmet>
-        <title>{post.title}</title>
-        <meta name="keywords" content={post.tags} />
-        <meta name="description" content={post.snippet} />
-        <meta name="author" content={post.author} />
-      </Helmet>
     </>
   );
 }
