60bda153fab4a65f235b705db04b356a19677d34
Braden Bell <fish0859@gmail.com>
2020-11-11 17:29:11 -0700

    remove build from gitignore

---
+++ client/build/precache-manifest.483e24807c48ce67dc9630ed817553d0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bd3b935c0df4fb8cb153760b03a232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "addd3f6aff2b8cff4020",
+    "url": "/static/css/main.24a7a5af.chunk.css"
+  },
+  {
+    "revision": "03fb95cea8461889b4a8",
+    "url": "/static/js/2.1b52de5c.chunk.js"
+  },
+  {
+    "revision": "906c8a35cf58ebf144e815a563375e87",
+    "url": "/static/js/2.1b52de5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a901e7e4d20e772973",
+    "url": "/static/js/3.087e1cfd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/3.087e1cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a641e399fb3b13441473",
+    "url": "/static/js/4.c35953f0.chunk.js"
+  },
+  {
+    "revision": "6353523a582eed51a4ab",
+    "url": "/static/js/5.28f67f98.chunk.js"
+  },
+  {
+    "revision": "a3bbac8b9980ba9df937",
+    "url": "/static/js/6.e78f6357.chunk.js"
+  },
+  {
+    "revision": "addd3f6aff2b8cff4020",
+    "url": "/static/js/main.aec309ee.chunk.js"
+  },
+  {
+    "revision": "7f55b7274ab5bfe9a192",
+    "url": "/static/js/runtime-main.96c9fb20.js"
+  }
+]);
